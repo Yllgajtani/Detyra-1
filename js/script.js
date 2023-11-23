5ec4f69269c28d8f782f13d483c704b030975dfb
Yllgajtani <gajtaniyll@gmail.com>
2023-11-23 07:40:58 -0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let number  = document.querySelector('.number')
+let result = document.querySelector('.result')
+let randomNr;
+
+function CheckNumber() { 
+    randomNr = Math.floor(Math.random() * 100)
+    number.innerHTML = randomNr;
+    
+    if (number.textContent >= 10) {
+        result.textContent = "Success"
+        result.classList.add("success")
+        result.classList.remove("error")
+    }
+    else  (number.textContent < 10) {
+        result.textContent = "Fail, please try again!"
+        result.classList.add("error")
+        result.classList.remove("success")
+    }
+   
+
+}
